ae0d544685dbbc5a54d58cd3aa94d1f8f2ff99b7
dpc10ster <dpc10ster@gmail.com>
2021-12-26 21:28:22 -0500

    got 3-fits working
    RJafroc rsmFormulae.R is a mess; cause me much trouble here as I was not using intrinsic vs. physical parameters consistently

--- R/compare-3-fits/Compare3ProperRocFits.R
+++ R/compare-3-fits/Compare3ProperRocFits.R
@@ -41,160 +41,108 @@ UtilBinCountsOpPts <- function(dataset, trt = 1, rdr = 1)
 
 # Compare three proper-ROC curve fitting models 
 Compare3ProperRocFits <- function(datasetNames,
-                                  startIndx = 1, 
-                                  endIndx = 14, 
+                                  f,
                                   saveProprocLrcFile = FALSE, 
                                   reAnalyze = FALSE)
 {
-  ####  DPC notes on updating the results 2/17/18
-  ####  First run PROPROC on all datasets (see book Chapter 20)
-  ####  1. ret14 <- Compare3ProperRocFits(saveProprocLrcFile = TRUE) 
-  ####     this generates 14 .lrc files in RJafroc
-  ####  2. Move these files to VmWareShared folder
-  ####  3. Start VmWare and Windows 8
-  ####  4. Start OR DBM MRMC, select .lrc file, select PROPROC AUC and RUN ALL
-  ####  5. Repeat for each dataset
-  ####  6. Move 2 files (ending with .lroc and PROPROC area pooled.csv) from 
-  ####     VmWareShared to MRMCRuns to appropriate subdirectories.
-  ####  7. Remove spaces in names of all "proproc area pooled.csv" files  
-  ####  8. ret14 <- Compare3ProperRocFits(reAnalyze = TRUE) 
-  ####     this generates new results files in RSM6
-  ####
-  # 
-  # Peter Philips showed me (06/21/19) that this line was causing testthat failures
-  # in function expect_known_output()
-  # 
-  
   options(warn = 2) # warnings AS errors
-  # NOTE added 6/25/19 - this is matched at exit with: 
-  # options(warn = 0) # warnings NOT as errors 
-  # datasetNames <-  c("TONY", "VD", "FR", 
-  #                 "FED", "JT", "MAG", 
-  #                 "OPT", "PEN", "NICO",
-  #                 "RUS", "DOB1", "DOB2", 
-  #                 "DOB3", "FZR")
-  if (!(startIndx %in% seq(1,14) && endIndx %in% seq(1,14))) stop("illegal values for startIndx and/ or endIndx")
-  allBinnedDatasets <- as.list(array(dim = endIndx - startIndx + 1))
-  allResults1 <- as.list(array(dim = endIndx - startIndx + 1))
-  allPlots <- as.list(array(dim = endIndx - startIndx + 1))
-  for (f in startIndx:endIndx) {
-    fileName <- datasetNames[f]
-    theData <- get(sprintf("dataset%02d", f)) # the datasets already exist as R objects
-    lesDistr <- UtilLesionDistr(theData) # RSM ROC fitting needs to know lesDistr
-    
-    # convert to HR ROC data; and remove negative infinities
-    if (theData$descriptions$type == "FROC") rocData <- DfFroc2Roc(theData) else rocData <- theData  
-    
-    if (saveProprocLrcFile) {
-      DfSaveDataFile(rocData, 
-                     fileName = paste0(fileName,".lrc"), format = "MRMC")
-    }
-    I <- length(rocData$descriptions$modalityID);J <- length(rocData$descriptions$readerID)
-    K <- dim(rocData$ratings$NL)[3];K2 <- dim(rocData$ratings$LL)[3];K1 <- K - K2
-    
-    ## retrieve PROPROC parameters
-    csvFileName <- paste0(fileName, "proprocareapooled.csv") # runs on July 29, 2017
-    sysCsvFileName <- paste0("R/compare-3-fits/MRMCRuns/",fileName, "/", csvFileName)
-    if (!file.exists(sysCsvFileName)) stop("need to run Windows PROPROC for this dataset using VMware Fusion")
-    proprocRet <- read.csv(sysCsvFileName)
-    c1 <- matrix(data = proprocRet$c, nrow = length(unique(proprocRet$T)), 
-                 ncol = length(unique(proprocRet$R)), byrow = TRUE)
-    da <- matrix(data = proprocRet$d_a, nrow = length(unique(proprocRet$T)), 
-                 ncol = length(unique(proprocRet$R)), byrow = TRUE)
-    
-    retFileName <- paste0("R/compare-3-fits/RSM6/", "allResults", fileName) 
-    if (!all(isBinnedDataset(rocData))){
-      binnedRocData <- DfBinDataset(rocData, desiredNumBins = 5, opChType = "ROC") # new function
-    }else{
-      binnedRocData <- rocData
-    }
-    
-    #cat(fileName,	" i, j, mu, lambdaP,	nuP, c,	da,	alpha, muCbm,	AUC-RSM, AUC-PROPROC, AUC-CBM, chisq, p-value,  df\n")
-    if (reAnalyze || !file.exists(retFileName)){
-      allResults <- list()
-      AllResIndx <- 0
-      compPlot <- array(list(), dim = c(I,J))
-      for (i in 1:I){
-        for (j in 1:J){
-          #if (!(f == 2 && i == 2 && j == 2)) next ## investigating warnings
-          AllResIndx <- AllResIndx + 1
-          retCbm <- FitCbmRoc(binnedRocData, trt = i, rdr = j)
-          retRsm <- FitRsmRoc(binnedRocData, trt = i, rdr = j, lesDistr = lesDistr[,2]) # fit to RSM, need lesDistr matrix
-          retCbm1 <- retCbm[-10] # deleting plots as they generate Notes in R CMD CHK -> file size too large
-          retRsm1 <- retRsm[-11] #   do:
-          aucProproc <- UtilAucPROPROC(c1[i,j], da[i,j])
-          allResults[[AllResIndx]] <- list(retRsm = retRsm1, retCbm = retCbm1, lesDistr = lesDistr, 
-                                           c1 = c1[i, j], da = da[i, j], aucProp = aucProproc, 
-                                           I = I, J = J, K1 = K1, K2 = K2)
-          x <- allResults[[AllResIndx]]
-          lesDistr <- x$lesDistr
-          empOp <- UtilBinCountsOpPts(binnedRocData, trt = i, rdr = j)
-          fpf <- empOp$fpf; tpf <- empOp$tpf
-          compPlot[[i,j]] <- gpfPlotRsmPropCbm(
-            datasetNames[which(datasetNames == fileName)], x$retRsm$mu, x$retRsm$lambdaP, x$retRsm$nuP, 
-            lesDistr, c1[i, j], da[i, j],
-            x$retCbm$mu, x$retCbm$alpha,
-            fpf, tpf, i, j, K1, K2, c(1, length(fpf)))
-          allPlots[[f-startIndx + 1]] <- compPlot
-          # follows same format as RSM Vs. Others.xlsx
-          # cat(fileName, i, j, x$retRsm$mu, x$retRsm$lambdaP, x$retRsm$nuP,
-          #     c1[i,j], da[i,j],
-          #     x$retCbm$alpha, x$retCbm$mu,
-          #     x$retRsm$AUC, x$aucProp, x$retCbm$AUC,
-          #     x$retRsm$ChisqrFitStats[[1]], x$retRsm$ChisqrFitStats[[2]],
-          #     x$retRsm$ChisqrFitStats[[3]],"\n")
-          next
-        }
+  fileName <- datasetNames[f]
+  theData <- get(sprintf("dataset%02d", f)) # the datasets already exist as R objects
+  lesDistr <- UtilLesionDistr(theData) # RSM ROC fitting needs to know lesDistr
+  
+  # convert to HR ROC data; and remove negative infinities
+  if (theData$descriptions$type == "FROC") rocData <- DfFroc2Roc(theData) else rocData <- theData  
+  
+  if (saveProprocLrcFile) {
+    DfSaveDataFile(rocData, 
+                   fileName = paste0(fileName,".lrc"), format = "MRMC")
+  }
+  
+  I <- length(rocData$descriptions$modalityID)
+  J <- length(rocData$descriptions$readerID)
+  K <- dim(rocData$ratings$NL)[3]
+  K2 <- dim(rocData$ratings$LL)[3];K1 <- K - K2
+  
+  ## retrieve PROPROC parameters
+  csvFileName <- paste0(fileName, "proprocareapooled.csv") # runs on July 29, 2017
+  sysCsvFileName <- paste0("R/compare-3-fits/MRMCRuns/",fileName, "/", csvFileName)
+  if (!file.exists(sysCsvFileName)) stop("need to run Windows PROPROC for this dataset using VMware Fusion")
+  proprocRet <- read.csv(sysCsvFileName)
+  c1 <- matrix(data = proprocRet$c, nrow = length(unique(proprocRet$T)), 
+               ncol = length(unique(proprocRet$R)), byrow = TRUE)
+  da <- matrix(data = proprocRet$d_a, nrow = length(unique(proprocRet$T)), 
+               ncol = length(unique(proprocRet$R)), byrow = TRUE)
+
+  retFileName <- paste0("R/compare-3-fits/RSM6/", "allResults", fileName)
+  
+  if (!all(isBinnedDataset(rocData))){
+    binnedRocData <- DfBinDataset(rocData, desiredNumBins = 5, opChType = "ROC") # new function
+  }else{
+    binnedRocData <- rocData
+  }
+  
+  compPlot_ij <- array(list(), dim = c(I, J))
+  allResults_ij <- array(list(), dim = c(I, J))
+  
+  if (reAnalyze || !file.exists(retFileName)){
+    AllResIndx <- 0
+    for (i in 1:I){
+      for (j in 1:J){
+        AllResIndx <- AllResIndx + 1
+        ret_C <- FitCbmRoc(binnedRocData, trt = i, rdr = j)
+        ret_R <- FitRsmRoc(binnedRocData, trt = i, rdr = j, lesDistr = lesDistr[,2]) # fit to RSM, need lesDistr matrix
+        retCbm <- ret_C[-10] # deleting plots as they generate Notes in R CMD CHK -> file size too large
+        retRsm <- ret_R[-11] #   do:
+        aucProproc <- UtilAucPROPROC(c1[i,j], da[i,j])
+        allResults_ij[[i,j]] <- list(retRsm = retRsm, retCbm = retCbm, lesDistr = lesDistr, 
+                                         c1 = c1[i, j], da = da[i, j], aucProp = aucProproc, 
+                                         I = I, J = J, K1 = K1, K2 = K2)
+        x <- allResults_ij[[i,j]]
+        lesDistr <- x$lesDistr
+        empOp <- UtilBinCountsOpPts(binnedRocData, trt = i, rdr = j)
+        fpf <- empOp$fpf; tpf <- empOp$tpf
+        compPlot_ij[[i, j]] <- gpfPlotRsmPropCbm(
+          fileName, x$retRsm$mu, x$retRsm$lambdaP, x$retRsm$nuP, 
+          lesDistr, c1[i, j], da[i, j],
+          x$retCbm$mu, x$retCbm$alpha,
+          fpf, tpf, i, j, K1, K2, c(1, length(fpf)))
+        next
       }
-      allResults1[[f-startIndx + 1]] <- allResults
-      allBinnedDatasets[[f-startIndx + 1]] <- binnedRocData
-      # cat("\n")
-      ### safety comments
-      ### to update allResults, make sure correct path is defined below
-      ### in git version it is rjafroc; in CRAN version it is rjafroc
-      ### uncomment the following two statements and run ret <- Compare3ProperRocFits(reAnalyze = TRUE)
-      ### the sytem files are updated on next build (the writes occur to savFileName, which are used to update system files) 
-      # savFileName <- paste0("RSM6/", retFileName) # git version
-      # save(allResults, file = savFileName)
-    } else {
-      load(retFileName)
-      AllResIndx <- 0
-      #cat(fileName,	" i, j, mu, lambdaP,	nuP, c,	da,	alpha, muCbm,	AUC-RSM, AUC-PROPROC, AUC-CBM, chisq, p-value,  df\n")
-      compPlot <- array(list(), dim = c(I,J))
-      for (i in 1:I){
-        for (j in 1:J){
-          AllResIndx <- AllResIndx + 1
-          x <- allResults[[AllResIndx]]
-          empOp <- UtilBinCountsOpPts(binnedRocData, trt = i, rdr = j)
-          fpf <- empOp$fpf; tpf <- empOp$tpf
-          compPlot[[i,j]]  <- gpfPlotRsmPropCbm(
-            datasetNames[which(datasetNames == fileName)], x$retRsm$mu, x$retRsm$lambdaP, x$retRsm$nuP, 
-            lesDistr, c1[i, j], da[i, j],
-            x$retCbm$mu, x$retCbm$alpha,
-            fpf, tpf, i, j, K1, K2, c(1, length(fpf)))
-          allPlots[[f-startIndx + 1]] <- compPlot
-          # follows same format as RSM Vs. Others.xlsx
-          # cat(fileName, i, j, x$retRsm$mu, x$retRsm$lambdaP, x$retRsm$nuP, 
-          #     c1[i,j], da[i,j], 
-          #     x$retCbm$alpha, x$retCbm$mu,
-          #     x$retRsm$AUC, x$aucProp, x$retCbm$AUC, 
-          #     x$retRsm$ChisqrFitStats[[1]], x$retRsm$ChisqrFitStats[[2]], 
-          #     x$retRsm$ChisqrFitStats[[3]],"\n")
-          next
-        }
+    }
+    
+    ### safety comments
+    ### to update allResults_ij, make sure correct path is defined below
+    ### in git version it is rjafroc; in CRAN version it is rjafroc
+    ### uncomment the following two statements and run ret <- Compare3ProperRocFits(reAnalyze = TRUE)
+    ### the sytem files are updated on next build (the writes occur to savFileName, which are used to update system files) 
+    # savFileName <- paste0("/Users/Dev/rjafroc/inst/ANALYZED/RSM6/", retFileName) # git version
+    # save(allResults, file = savFileName)
+  } else {
+    load(retFileName)
+    AllResIndx <- 0
+    for (i in 1:I){
+      for (j in 1:J){
+        AllResIndx <- AllResIndx + 1
+        x <- allResults[[AllResIndx]]
+        allResults_ij[[i,j]] <- x
+        empOp <- UtilBinCountsOpPts(binnedRocData, trt = i, rdr = j)
+        fpf <- empOp$fpf; tpf <- empOp$tpf
+        compPlot_ij[[i,j]] <- gpfPlotRsmPropCbm(
+          fileName, x$retRsm$mu, x$retRsm$lambdaP, x$retRsm$nuP, 
+          lesDistr, c1[i, j], da[i, j],
+          x$retCbm$mu, x$retCbm$alpha,
+          fpf, tpf, i, j, K1, K2, c(1, length(fpf)))
+        next
       }
-      allResults1[[f-startIndx + 1]] <- allResults
-      allBinnedDatasets[[f-startIndx + 1]] <- binnedRocData
-      # cat("\n\n\n")
     }
   }
   
   options(warn = 0) # warnings NOT as errors
   
   return(list(
-    allResults = allResults1,
-    allBinnedDatasets = allBinnedDatasets,
-    allPlots = allPlots
+    allPlots = compPlot_ij,
+    allResults = allResults_ij
+    #allBinnedDatasets <- binnedRocData
   ))
 }
 
@@ -203,38 +151,39 @@ Compare3ProperRocFits <- function(datasetNames,
 gpfPlotRsmPropCbm <- function(fileName, mu, lambdaP, nuP, lesDistr, c1, da, 
                               muCbm, alpha, fpf, tpf, i, j, K1, K2, ciIndx) {
   
-  retProproc <- gpfPropRocOperatingCharacteristic(c1,da)
-  FPFProp <- c(1, retProproc$FPF);TPFProp <- c(1, retProproc$TPF) # make sure it goes to upper-right corner
-  plotProp <- data.frame(FPF = FPFProp, TPF = TPFProp, Model = "PROP")
-  retRsm <- gpfRsmOperatingCharacteristic (mu, lambdaP, nuP, lesDistr = lesDistr[,2]) # dpc 1/1/2021
-  FPFRsm <- retRsm$FPF;TPFRsm <- retRsm$TPF
-  plotRsm <- data.frame(FPF = FPFRsm, TPF = TPFRsm, Model = "RSM")
-  dashedRsm <- data.frame(FPF = c(FPFRsm[1], 1), TPF = c(TPFRsm[1], 1), Model = "RSM")
+  ret_P <- gpfPropRocOperatingCharacteristic(c1,da)
+  FPF_P <- c(1, ret_P$FPF);TPF_P <- c(1, ret_P$TPF) # make sure it goes to upper-right corner
+  plotProp <- data.frame(FPF = FPF_P, TPF = TPF_P, Model = "PROP")
   
-  retCbm <- gpfCbmOperatingCharacteristic (muCbm, alpha)
-  FPFCbm <- retCbm$FPF;TPFCbm <- retCbm$TPF
-  plotCbm <- data.frame(FPF = FPFCbm, TPF = TPFCbm, Model = "CBM")
+  ret_R <- gpfRsmOperatingCharacteristic (mu, lambdaP, nuP, lesDistr = lesDistr[,2]) # dpc 1/1/2021
+  FPF_R <- ret_R$FPF;TPF_R <- ret_R$TPF
+  plotRsm <- data.frame(FPF = FPF_R, TPF = TPF_R, Model = "RSM")
+  dashedRsm <- data.frame(FPF = c(FPF_R[1], 1), TPF = c(TPF_R[1], 1), Model = "RSM")
+  
+  ret_C <- gpfCbmOperatingCharacteristic (muCbm, alpha)
+  FPF_C <- ret_C$FPF;TPF_C <- ret_C$TPF
+  plotCbm <- data.frame(FPF = FPF_C, TPF = TPF_C, Model = "CBM")
   
   plotCurve <- rbind(plotProp, plotCbm, plotRsm)
   plotCurve <- as.data.frame(plotCurve)
   plotOp <- data.frame(FPF = fpf, TPF = tpf)
   plotOp <- as.data.frame(plotOp)
   
-  ij <- paste0(fileName, ", i = ", i, ", j = ", j)
+  ij <- paste0("D", fileName, ", i = ", i, ", j = ", j)
   Model <- NULL # to get around R CMD CHK throwing a Note
   FPF <- fpf
   TPF <- tpf
   FPF <- FPF[ciIndx]
   TPF <- TPF[ciIndx]
   fitPlot <- ggplot(data = plotCurve) + 
-    geom_line(mapping = aes(x = FPF, y = TPF, color = Model)) + 
-    geom_line(data = dashedRsm, aes(x = FPF, y = TPF, color = Model), linetype = 3) + 
+    geom_line(mapping = aes(x = FPF, y = TPF, color = Model), size = 2) + 
+    geom_line(data = dashedRsm, aes(x = FPF, y = TPF, color = Model), linetype = 3, size = 2) + 
     scale_color_manual(values = c("red", "darkblue", "black")) # color corresponds to order of plots in plotCurve
-  
+
   fitPlot <- fitPlot + 
-    geom_point(mapping = aes(x = FPF, y = TPF), data = plotOp) +
+    geom_point(mapping = aes(x = FPF, y = TPF), data = plotOp, size = 5) +
     theme(legend.position = "none") + 
-    ggtitle(ij)# + theme(plot.title = element_text(size = 0.5,face="bold"))
+    ggtitle(ij) + theme(plot.title = element_text(size = 20,face="bold"))
   
   ciX <- binom.confint(x = FPF * K1, n = K1, methods = "exact")
   ciY <- binom.confint(x = TPF * K2, n = K2, methods = "exact")
@@ -268,28 +217,30 @@ gpfPropRocOperatingCharacteristic <- function(c1, da){
   }else{
     plotZeta <- seq(-4, 10, by = 0.01)
   }
-  FPFProp <- pnorm(-(1 - c1) * plotZeta - da/2 * sqrt(1 + c1^2)) + 
+  FPF_P <- pnorm(-(1 - c1) * plotZeta - da/2 * sqrt(1 + c1^2)) + 
     pnorm(-(1 - c1) * plotZeta + da/(2*c1) * sqrt(1 + c1^2)) - ifelse(c1 >= 0, 1, 0)
-  TPFProp <- pnorm(-(1 + c1) * plotZeta + da/2 * sqrt(1 + c1^2)) + 
+  TPF_P <- pnorm(-(1 + c1) * plotZeta + da/2 * sqrt(1 + c1^2)) + 
     pnorm(-(1 + c1) * plotZeta + da/(2*c1) * sqrt(1 + c1^2)) - ifelse(c1 >= 0, 1, 0)
   if ((c1 == 1) && (da == 0)) {
-    FPFProp <- c(FPFProp, seq(0,1,length.out = length(FPFProp)))
-    TPFProp <- c(TPFProp, rep(1,length(TPFProp)))
+    FPF_P <- c(FPF_P, seq(0,1,length.out = length(FPF_P)))
+    TPF_P <- c(TPF_P, rep(1,length(TPF_P)))
   }
-  return(list(FPF = FPFProp,
-              TPF = TPFProp
+  return(list(FPF = FPF_P,
+              TPF = TPF_P
   ))
 }
+
+
 
 gpfCbmOperatingCharacteristic <- function(mu, alpha){
   
   plotZeta <- seq(-4, mu+4, by = 0.1)
   
-  FPFCbm <- 1 - pnorm(plotZeta)
-  TPFCbm <- (1 - alpha) * (1 - pnorm(plotZeta)) + alpha * (1 - pnorm(plotZeta, mean = mu))
+  FPF_C <- 1 - pnorm(plotZeta)
+  TPF_C <- (1 - alpha) * (1 - pnorm(plotZeta)) + alpha * (1 - pnorm(plotZeta, mean = mu))
   
-  return(list(FPF = FPFCbm,
-              TPF = TPFCbm
+  return(list(FPF = FPF_C,
+              TPF = TPF_C
   ))
 }
 
@@ -297,17 +248,14 @@ gpfCbmOperatingCharacteristic <- function(mu, alpha){
 
 gpfRsmOperatingCharacteristic <- function(mu, lambdaP, nuP, lesDistr){
   
-  x <- UtilPhysical2IntrinsicRSM(mu,lambdaP,nuP)
-  lambda <- x$lambda
-  nu <- x$nu
-  plotZeta <- seq(-4, mu+4, by = 0.01)
+  plotZeta <- seq(-4, mu+4, by = 0.1)
   
-  FPFRsm <- sapply(plotZeta, RSM_xROC, lambda = lambda)
-  TPFRsm <- sapply(plotZeta, RSM_yROC, mu = mu, lambda = lambda, 
-                   nu = nu, lesDistr = lesDistr)
+  FPF_R <- sapply(plotZeta, RSM_xROC, lambdaP = lambdaP)
+  TPF_R <- sapply(plotZeta, RSM_yROC, mu = mu, lambdaP = lambdaP, 
+                  nuP = nuP, lesDistr = lesDistr)
   
-  return(list(FPF = FPFRsm,
-              TPF = TPFRsm
+  return(list(FPF = FPF_R,
+              TPF = TPF_R
   ))
 }
 
---
+++ index.md
@@ -0,0 +1,137 @@
+--- 
+title: "The RJafroc Froc Book"
+author: "Dev P. Chakraborty, PhD"
+date: "2021-12-26"
+site: bookdown::bookdown_site
+output: 
+   bookdown::pdf_document: default
+documentclass: book
+bibliography: [packages.bib, myRefs.bib]
+biblio-style: apalike
+link-citations: yes
+github-repo: dpc10ster/RJafrocFrocBook
+description: "Artificial intelligence and observer performance book based on RJafroc."
+---
+
+
+
+
+
+# Preface {-}
+
+* It is intended as an online update to my print book [@chakraborty2017observer]. 
+* Since its publication in 2017 the `RJafroc` package, on which the `R` code examples in the book depend, has evolved considerably, causing many of the examples to "break". 
+* This also gives me the opportunity to improve on the book and include additional material.
+* The online book is in 3 parts.
+* This part is devoted to the FROC paradigm.
+
+
+
+## TBA How much finished {-}
+
+* HMF approximately 30%
+* This book is currently (as of December 2021) in preparation. 
+* Parts labeled TBA and TODOLAST need to be updated on final revision.
+* Un-comment links like `\@ref(froc-paradigm-solar-analogy)` etc. Search for `\@ref
+
+
+
+## The pdf file of the book {-}
+
+Go [here](https://github.com/dpc10ster/RJafrocFrocBook/blob/gh-pages/RJafrocFrocBook.pdf) and then click on `Download` to get the `RJafrocFrocBook.pdf` file.
+
+
+## The html version of the book {-}
+Go [here](https://dpc10ster.github.io/RJafrocFrocBook/) to view the `html` version of the book.
+
+
+## A note on the online distribution mechanism of the book {-}
+* In the hard-copy version of my book [@chakraborty2017observer] the online distribution mechanism was `BitBucket`. 
+* `BitBucket` allows code sharing within a _closed_ group of a few users (e.g., myself and a grad student). 
+* Since the purpose of open-source code is to encourage collaborations, this was, in hindsight, an unfortunate choice. Moreover, as my experience with R-packages grew, it became apparent that the vast majority of R-packages are shared on `GitHub`, not `BitBucket`. 
+* For these reasons I have switched to `GitHub`. All previous instructions pertaining to `BitBucket` are obsolete.
+* In order to access `GitHub` material one needs to create a (free) `GitHub` account. 
+* Go to [this link](https://github.com) and click on `Sign Up`.
+
+
+## Structure of the book {-}
+The book is divided into parts as follows:
+
+* Part I: Quick Start: intended for existing Windows `JAFROC` users who are seeking a quick-and-easy transition from Windows `JAFROC` to `RJafroc`. 
+* Part II: ROC paradigm: this covers the basics of the ROC paradigm 
+* Part III: Significance Testing: The general procedure used to determine the significance level, and associated statistics, of the observed difference in figure of merit between pairs of treatments or readers
+* Part IV: FROC paradigm: TBA
+
+
+## Contributing to this book {-}
+
+I appreciate constructive feedback on this document. To do this raise an `Issue` on the `GitHub` [interface](https://github.com/dpc10ster/RJafrocFrocBook). Click on the `Issues` tab under `dpc10ster/RJafrocFrocBook`, then click on `New issue`. When done this way, contributions from users automatically become part of the `GitHub` documentation/history of the book.
+
+
+
+## Is this book relevant to you and what are the alternatives? {-}
+
+* Diagnostic imaging system evaluation
+* Detection
+* Detection combined with localization
+* Detection combined with localization and classification
+* Optimization of Artificial Intelligence (AI) algorithms
+* CV
+* Alternatives
+
+
+
+
+## Chapters needing heavy edits {-}
+
+* 12-froc.
+* 13-empirical.
+* 13-empirical-examples.
+
+
+## Shelved vs. removed vs. parked folders needing heavy edits {-}
+
+* replace functions with \text{}; eg. erf and exp in all of document
+* Also for TPF, FPF etc.
+* Temporarily shelved 17c-rsm-evidence.Rmd in removed folder
+* Now 17-b is breaking; possibly related to changes in RJafroc: had to do with recent changes to RJafroc code - RSM_xFROC etc requiring intrinsic parameters; fixed 17-b
+* parked has dependence of ROC/FROC performance on threshold
+
+
+## Coding aids (for me) {-}
+
+* weird error with knitr not responding to changes in Rmd file: traced to upper case lower case confusion: 13A-empirical1.Rmd which should be 13a-empirical1.Rmd
+
+### formatting
+* sprintf("%.4f", proper formatting of numbers
+* OpPtStr(, do:
+
+### tables
+
+* https://github.com/haozhu233/kableExtra/issues/624
+* kbl(dfA, caption = "....", booktabs = TRUE, escape = FALSE) %>% collapse_rows(columns = c(1, 3), valign = "middle") %>% kable_styling(latex_options = c("basic", "scale_down", "HOLD_position"), row_label_position = "c") 
+* ```{r, attr.source = ".numberLines"}
+* kbl(x12, caption = "Summary of optimization results using wAFROC-AUC.", booktabs = TRUE, escape = FALSE) %>% collapse_rows(columns = c(1), valign = "middle") %>% kable_styling(latex_options = c("basic", "scale_down", "HOLD_position"), row_label_position = "c") 
+* $\text{exp} \left ( -\lambda' \right )$ space before dollar sign generates a pdf error 
+* FP errors generated by GitHub actions due to undefined labels:
+Error: Error: pandoc version 1.12.3 or higher is required and was not found (see the help page ?rmarkdown::pandoc_available).
+In addition: Warning message:
+In verify_rstudio_version() :
+Please install or upgrade Pandoc to at least version 1.17.2; or if you are using RStudio, you can just install RStudio 1.0+.
+Execution halted
+
+### tinytex problems
+
+* dont update in response to messages; breaks everything
+* DONT DO THIS: When tinytex::install_tinytex() hangs up try
+* DONT DO THIS: tinytex::install_tinytex(repository = "http://mirrors.tuna.tsinghua.edu.cn/CTAN/", version = "latest")
+* Getting very long builds: looping certain commands
+* First uninstall tinytex then reinstall:
+
+```r
+#uninstall_tinytex(force = FALSE, dir = tinytex_root())
+#tinytex::install_tinytex()
+```
+* get very long build first time with looping certain commands
+* fixed on subsequent pdf builds
+
