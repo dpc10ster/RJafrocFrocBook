23f76e3b73ce650f86ae3ce50853182e06060df0
dpc10ster <dpc10ster@gmail.com>
2022-11-04 10:10:22 -0400

    cleanup; using `source` to cut down on inline R code

---
+++ R/optim-op-point/doOneTable.R
@@ -0,0 +1,8 @@
+ret <- doOneSet(muArr, lambdaArr, nuArr, lesDistr, relWeights)
+zetaOptArr <- ret$zetaOptArr
+nlfOptArr <- ret$nlfOptArr
+llfOptArr <- ret$llfOptArr
+fpfOptArr <- ret$fpfOptArr
+tpfOptArr <- ret$tpfOptArr
+wAfrocArr <- ret$wAfrocArr
+rocArr <- ret$rocArr
